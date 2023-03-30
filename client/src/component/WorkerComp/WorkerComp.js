91c9ea05519f9b510532bde6068258253c917d14
kellyatnuk <kellyatnuk@gmail.com>
2023-03-30 12:05:36 +0600

    changes in workerpage

--- client/src/component/WorkerComp/WorkerComp.js
+++ client/src/component/WorkerComp/WorkerComp.js
@@ -18,38 +18,38 @@ export default function WorkerComp() {
           <tr>
             <td>1</td>
             <td>Issue 1</td>
-          <button type="checkbox" id="" value="" className='btn btn-primary mb-2 mt-2'>Solved</button>
+          <button className='btn btn-primary mb-2 mt-2'>Solved</button>
           </tr>
           <tr>
             <td>2</td>
             <td>Issue 2</td>
-          <button type="checkbox" id="" className='btn btn-primary mb-2' value="">Solved</button>
+          <button className='btn btn-primary mb-2'>Solved</button>
           </tr>
           <tr>
             <td>3</td>
             <td>Issue 3</td>
-          <button type="checkbox" id="" className='btn btn-primary mb-2' value="">Solved</button>
+          <button className='btn btn-primary mb-2'>Solved</button>
           </tr>
           <tr>
             <td>4</td>
             <td>Issue 4</td>
-          <button type="checkbox" id="" className='btn btn-primary mb-2' value="">Solved</button>
+          <button  className='btn btn-primary mb-2'>Solved</button>
           
           </tr>
           <tr>
             <td>5</td>
             <td>Issue 5</td>
-          <button type="checkbox" id="" className='btn btn-primary mb-2' value="">Solved</button>
+          <button  className='btn btn-primary mb-2'>Solved</button>
           </tr>
           <tr>
             <td>6</td>
             <td>Issue 6</td>
-          <button type="checkbox" id="" className='btn btn-primary mb-2' value="">Solved</button>
+          <button className='btn btn-primary mb-2'>Solved</button>
           </tr>
           <tr>
             <td>7</td>
             <td>Issue 7</td>
-          <button type="checkbox" id="" className='btn btn-primary mb-2' value="">Solved</button>
+          <button className='btn btn-primary mb-2'>Solved</button>
           </tr>
         </tbody>
       </table>
