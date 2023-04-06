6a68115f3f6d19e0000ef49d6da665d6f23278fa
Bishal Dhakal <dhakalbishal930@.com>
2023-04-06 20:54:33 +0600

    app retrived

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import NavbarComp from "./component/NavbarComp/NavbarComp";
+import HomeComp from "./component/User/HomeComp/HomeComp";
+import LoginComp from "./component/LoginComp/LoginComp";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AdminHome from "./component/Admin/AdminHome/AdminHome";
+import CheckComp from "./component/CheckComp/CheckComp";
+import WorkerComp from "./component/WorkerComp/WorkerComp";
+import EditComp from "./component/Admin/EditComp/EditComp";
+import AddWorker from "./component/Admin/AddWorker/AddWorker";
+import DeleteWorkerComp from "./component/Admin/DeleteWorkerComp/DeleteWorkerComp";
+import ViewerHome from "./component/Viewer/ViewerHome";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <NavbarComp />
+        <Routes>
+          <Route path="/" element={<HomeComp />} />
+          <Route path="/login" element={<LoginComp />} />
+          <Route path="/admin" element={<AdminHome />} />
+          <Route path="/check" element={<CheckComp />} />
+          <Route path="/worker" element={<WorkerComp />} />
+          <Route path="/edit" element={<EditComp />} />
+          <Route path="/add" element={<AddWorker />} />
+          <Route path="/delete" element={<DeleteWorkerComp />} />
+          <Route path="/view" element={<ViewerHome />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
