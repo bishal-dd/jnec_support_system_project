580dee5637beccf396eadc165444a729f99a3424
Bishal Dhakal <dhakalbishal930@.com>
2023-04-06 21:23:48 +0600

    git ignore added

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 const mysql = require("mysql2");
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
