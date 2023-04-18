7d93686dc49b1bd55f9980841bb5a73324970d2b
Bishal Dhakal <dhakalbishal930@.com>
2023-04-18 21:12:38 +0600

    graph added to viewer

--- client/src/component/Viewer/ViewerHome.js
+++ client/src/component/Viewer/ViewerHome.js
@@ -1,5 +1,65 @@
 import React from "react";
+import { PieChart, Pie, Legend, Tooltip, Cell } from "recharts";
 
 export default function ViewerHome() {
-  return <div> something</div>;
+  const data = [
+    { name: "Apple", value: 400 },
+    { name: "Banana", value: 300 },
+    { name: "Orange", value: 200 },
+  ];
+
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <PieChart width={400} height={400}>
+            <Pie
+              data={data}
+              dataKey="value"
+              nameKey="name"
+              cx="50%"
+              cy="50%"
+              outerRadius={80}
+              fill="#8884d8"
+              label
+            >
+              {data.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+            <Tooltip />
+            <Legend />
+          </PieChart>
+        </div>
+        <div className="col">
+          <PieChart width={400} height={400}>
+            <Pie
+              data={data}
+              dataKey="value"
+              nameKey="name"
+              cx="50%"
+              cy="50%"
+              outerRadius={80}
+              fill="#8884d8"
+              label
+            >
+              {data.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index % COLORS.length]}
+                />
+              ))}
+            </Pie>
+            <Tooltip />
+            <Legend />
+          </PieChart>
+        </div>
+      </div>
+    </div>
+  );
 }
